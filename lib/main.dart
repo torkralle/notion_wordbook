c495876dd846fd8998784707dfe0288c32d8019f
torkralle <torkralle@gmail.com>
2022-02-21 11:43:48 +0900

    マージミスってたので修正

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
 import 'package:notion_wordbook/screens/connecting.dart';
-
+import 'package:notion_wordbook/widgets/bottom_navbar.dart';
 // 🌎 Project imports:
 
 void main() {
