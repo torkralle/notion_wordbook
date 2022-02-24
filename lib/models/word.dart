1424aaba5ed6f55cf35809e2e568a47c1dd0304b
USK314 <usk31101021@gmail.com>
2022-02-24 23:14:21 +0900

    mainから引き継いだエラーを解消した

--- lib/models/word.dart
+++ lib/models/word.dart
@@ -1,18 +1,1 @@
-import 'package:json_annotation/json_annotation.dart';
 
-part 'word.g.dart';
-
-//キャメルケースを、Jsonでスネークケースに変換
-@JsonSerializable(fieldRename: FieldRename.snake)
-class Word {
-  String spelling;
-  String meaning;
-  String link;
-  bool correct;
-
-  Word({required this.spelling});
-
-  factory Word.fromJson(Map<String, dynamic> json) => _$WordFromJson(json);
-
-  Map<String, dynamic> toJson() => _$WordToJson(this);
-}
