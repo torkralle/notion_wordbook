d915a2f448107aabbd7a1a0d995ca20a9dba4743
USK314 <usk31101021@gmail.com>
2022-04-07 18:50:15 +0900

    fix: cardを別widgetに分け、wordbookオブジェクトからrankを排除

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -4,13 +4,14 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   HomePage({Key? key}) : super(key: key);
 
-  final _wordbook = <Map<String, dynamic>>[
-    {'rank': 'A', 'listName': 'はじめての英語', 'ratio': '23.4'},
-    {'rank': 'B', 'listName': '基礎からの英語', 'ratio': '78.2'},
-    {'rank': 'C', 'listName': '日常英語', 'ratio': '1.1'},
-    {'rank': 'A', 'listName': '高校英語', 'ratio': '45.3'},
-    {'rank': 'D', 'listName': 'NewYorkTimes', 'ratio': '0.5'},
-    {'rank': 'D', 'listName': 'CNN', 'ratio': '0'},
+  final wordbook = <Map<String, dynamic>>[
+    {'listName': 'はじめての英語', 'ratio': '23.4'},
+    {'listName': '基礎からの英語', 'ratio': '78.2'},
+    {'listName': '日常英語', 'ratio': '1.1'},
+    {'listName': '高校英語', 'ratio': '45.3'},
+    {'listName': 'NewYorkTimes', 'ratio': '0.5'},
+    {'listName': 'CNN', 'ratio': '0'},
+    {'listName': 'Academic', 'ratio': '52.7'} 
   ];
 
   @override
@@ -55,62 +56,78 @@ class HomePage extends StatelessWidget {
               ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
-                itemCount: _wordbook.length,
+                itemCount: wordbook.length,
                 itemBuilder: (BuildContext context, index) {
-                  return Card(
-                    margin: const EdgeInsets.symmetric(
-                      vertical: 7,
-                      horizontal: 20,
-                    ),
-                    elevation: 2,
-                    color: const Color.fromARGB(234, 250, 241, 252),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.of(context).pushNamed('/test');
-                      },
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ListTile(
-                          title: Text(
-                            _wordbook[index]['listName'],
-                            style: const TextStyle(
-                              fontSize: 19,
-                            ),
-                          ),
-                          subtitle: Padding(
-                            padding: const EdgeInsets.only(top: 5),
-                            child: Text(
-                              '前回正答率 ' + _wordbook[index]['ratio'],
-                              style: const TextStyle(
-                                fontSize: 15,
-                              ),
-                            ),
-                          ),
-                          leading: const Padding(
-                            padding: EdgeInsets.symmetric(vertical: 12.0),
-                            child: Icon(
-                              Icons.circle_sharp,
-                              color: Colors.white,
-                              size: 20,
-                            ),
-                          ),
-                          trailing: InkWell(
-                            onTap: () {
-                              Navigator.of(context).pushNamed('/wordbook_item');
-                            },
-                            child: const Icon(Icons.more_horiz),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
+                  return BookCard(index: index, wordbook: wordbook);
                 },
               ),
             ],
           ),
         ),
       );
+}
+
+class BookCard extends StatelessWidget {
+  const BookCard({
+    Key? key,
+    required this.index,
+    required this.wordbook,
+  }) : super(key: key);
+
+  final int index;
+  final List<Map<String, dynamic>> wordbook;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(
+        vertical: 7,
+        horizontal: 20,
+      ),
+      elevation: 2,
+      color: const Color.fromARGB(234, 250, 241, 252),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: InkWell(
+        onTap: () {
+          Navigator.of(context).pushNamed('/test');
+        },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListTile(
+            title: Text(
+              wordbook[index]['listName'],
+              style: const TextStyle(
+                fontSize: 19,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.only(top: 5),
+              child: Text(
+                '前回正答率 ' + wordbook[index]['ratio'],
+                style: const TextStyle(
+                  fontSize: 15,
+                ),
+              ),
+            ),
+            leading: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 12.0),
+              child: Icon(
+                Icons.circle_sharp,
+                color: Colors.white,
+                size: 20,
+              ),
+            ),
+            trailing: InkWell(
+              onTap: () {
+                Navigator.of(context).pushNamed('/wordbook_item');
+              },
+              child: const Icon(Icons.more_horiz),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 }
