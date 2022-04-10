d5d62a8f2291223815be92c2cdb7f3e461032302
USK314 <usk31101021@gmail.com>
2022-04-10 18:37:38 +0900

    feat: notifierとproviderを作成

---
+++ lib/viewmodels/page_controllers.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notion_wordbook/viewmodels/word_list_controller.dart';
+
+class MaxPageNotifier extends StateNotifier<int> {
+  MaxPageNotifier(this.ref) : super(1);
+  Ref ref;
+
+  getListLength() async {
+    final wordMap = ref.watch(wordListStateProvider);
+    state = wordMap.length;
+  }
+}
+
+final maxPageProvider = StateNotifierProvider<MaxPageNotifier, int>((ref) {
+  return MaxPageNotifier(ref);
+});
+
+class CurrentPageNotifier extends StateNotifier<int> {
+  CurrentPageNotifier() : super(1);
+
+  void increasePage() => state++;
+}
+
+final currentPageProvider =
+    StateNotifierProvider<CurrentPageNotifier, int>((ref) {
+  return CurrentPageNotifier();
+});
---
+++ lib/viewmodels/word_list_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notion_wordbook/client/words/main.dart';
+import 'package:notion_wordbook/screens/hook_test.dart';
+
+class WordListNotifier extends StateNotifier<Map> {
+  WordListNotifier() : super({});
+
+  Future getWordList() async {
+    final wordListResult = await getWordsData(dbKey ?? '');
+    final wordList = wordListResult.body;
+
+    state = wordList ?? {};
+  }
+}
+
+final wordListStateProvider =
+    StateNotifierProvider<WordListNotifier, Map>((ref) {
+  return WordListNotifier();
+});
