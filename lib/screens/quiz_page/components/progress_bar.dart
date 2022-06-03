4267c8b5b960b3cd57992d8f5ed02d4ea88baf32
USK314 <usk31101021@gmail.com>
2022-06-03 12:44:05 +0900

    feat: progress barの動的表示

--- lib/screens/quiz_page/components/progress_bar.dart
+++ lib/screens/quiz_page/components/progress_bar.dart
@@ -1,18 +1,22 @@
 // 🐦 Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notion_wordbook/viewmodels/page_controllers.dart';
 
-class ProgressBar extends StatelessWidget {
+class ProgressBar extends ConsumerWidget {
   const ProgressBar({
     Key? key,
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return const LinearProgressIndicator(
-      value: 0.5,
+  Widget build(BuildContext context, WidgetRef ref) {
+    final currentPage = ref.watch(currentPageProvider);
+    final maxPage = ref.watch(maxPageProvider);
+    return LinearProgressIndicator(
+      value: currentPage / maxPage,
       minHeight: 5,
-      color: Color.fromARGB(255, 112, 33, 181),
-      backgroundColor: Color.fromARGB(255, 244, 235, 248),
+      color: const Color.fromARGB(255, 112, 33, 181),
+      backgroundColor: const Color.fromARGB(255, 244, 235, 248),
     );
   }
 }
