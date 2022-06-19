535c733227ff4d7182b082e6eb24a81b90f0712b
noriHanda <noriakihanda@gmail.com>
2022-06-19 20:58:18 +0900

    Revert "Remove unused .env import lines"
    This reverts commit 0f4f269c3eb4fed42a03dd22531ddc6047cf2e1b.

--- lib/viewmodels/word_list_controller.dart
+++ lib/viewmodels/word_list_controller.dart
@@ -1,15 +1,17 @@
 // 📦 Package imports:
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notion_wordbook/client/words/main.dart';
+
 // 🌎 Project imports:
 import 'package:notion_wordbook/helper/words/new_list.dart';
 import 'package:notion_wordbook/objects/models/word.dart';
+import 'package:notion_wordbook/screens/hook_test_page.dart';
 
 class WordListNotifier extends StateNotifier<List<Word>> {
   WordListNotifier() : super([]);
 
   Future readWordList() async {
-    final ApiResult wordListResult = await getWordsData('');
+    final ApiResult wordListResult = await getWordsData(dbKey ?? '');
     final List<Word> wordList = newWordsList(wordListResult.body!);
 
     state = wordList;
@@ -22,7 +24,7 @@ final wordListStateProvider =
 });
 
 final wordListFutureProvider = FutureProvider<List<Word>>((ref) async {
-  final ApiResult wordListResult = await getWordsData('');
+  final ApiResult wordListResult = await getWordsData(dbKey ?? '');
   final List<Word> wordList = newWordsList(wordListResult.body!);
 
   return wordList;
